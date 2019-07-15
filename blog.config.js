4de6a54ecd7be13d060cc8a2b50e14c2e380dda7
X Monkee <xmonkee@localhost.localdomain>
2019-07-14 21:03:10 -0700

    First commit

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'xmonkee',
+    author: 'Mayank Mandava',
+    image: '/static/site-feature.png',
+    description: 'xmonkee\'s personal blog',
+    siteUrl: 'https://xmonkee.now.sh',
+    social: {
+      twitter: 'xmonkee'
+    },
+    postsPerPage: 5
+  }
+}
---
+++ components/footer.js
@@ -0,0 +1,24 @@
+import Profile from './profile'
+
+function Footer () {
+  return (
+    <footer>
+      <Profile className='profile-footer' />
+
+      <p>
+        Proudly built with <a href='https://nextjs.org'>Next.js</a>
+      </p>
+      <style jsx>{`
+        footer {
+          padding: 1em 0;
+        }
+
+        p {
+          margin-top: 2em;
+        }
+      `}</style>
+    </footer>
+  )
+}
+
+export default Footer
